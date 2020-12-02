a86ff961d1d68b537a1cde16a5b20a9979dd02b7
LUXIANZE <luxianze98@gmail.com>
2020-12-02 12:27:16 +0800

    Coded the fragments and added click listener in MainActivity.java

--- app/src/main/java/com/example/lab6part2/MainActivity.java
+++ app/src/main/java/com/example/lab6part2/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.lab6part2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button BtnPenguin = (Button)findViewById(R.id.BtnPenguin);
+        Button BtnRabbit = (Button)findViewById(R.id.BtnRabbit);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = null;
+                if (v == findViewById(R.id.BtnPenguin)){
+                    fragment = new FragmentPenguin();
+                }else{
+                    fragment = new FragmentRabbit();
+                }
+                FragmentManager manager = getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                transaction.replace(R.id.FragOutput, fragment);
+                transaction.commit();
+            }
+        };
+
+        BtnPenguin.setOnClickListener(listener);
+        BtnRabbit.setOnClickListener(listener);
     }
 }
